2f65cd7764ec4752220a1ee56ee39929b7d2fe0c
khenson3 <khenson@multidataservices.com>
2025-07-23 17:39:43 -0400

    Initial commit Add file structure and basic code

---
+++ main.js
@@ -0,0 +1,27 @@
+// constructor for todo item object
+// properties: title, description, dueDate, priority, completed
+function item(title,description,dueDate,priority,completed) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.completed = completed;
+}
+
+// constructor for projects
+
+
+// dynamically insert new items into the taskList div, keyed by project
+
+
+// move completed items into the completedList per project
+
+
+// switch project displayed by dynamically changing what is displayed in taskList
+
+
+// delete todo
+
+
+// change priority of todo (options: normal or high(checked))
+
